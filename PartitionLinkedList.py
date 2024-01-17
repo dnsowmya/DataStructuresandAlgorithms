cc2a1df9a921d1140339b7154b631b31da7725ba
Sowmya Devarapu <d.n.sowmya@gmail.com>
2024-01-17 10:46:45 -0600

    LinkedList LeetCode Questions

---
+++ PartitionLinkedList.py
@@ -0,0 +1,22 @@
+# Definition for Singly Linked List
+# class ListNode:
+#   def __init__(self, val=0, next=None):
+#       self.val = val
+#       self.next = next
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        left, right = ListNode(), ListNode()
+        ltail, rtail = left, right
+
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+
+        ltail.next = right.next
+        rtail.next = None
+        return left.next
